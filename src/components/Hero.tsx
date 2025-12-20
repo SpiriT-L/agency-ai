e863deb54250b467d9f8281b640acd2df3da9fbe
LeonID <lmatskevich79@gmail.com>
2025-12-20 23:25:46 +0300

    feat: remove unnecessary React import and enhance Hero component with new content and images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import assets from '../assets/assets';
 
 const Hero = () => {
@@ -10,6 +9,30 @@ const Hero = () => {
       <div className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
         <img className='w-20' src={assets.group_profile} alt='group profile' />
         <p className='text-xs font-medium'>Trusted by 10k+ people</p>
+      </div>
+
+      <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
+        Turning imagination into{' '}
+        <span className='bg-linear-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>
+          digital
+        </span>{' '}
+        impact.
+      </h1>
+      <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>
+        Creating meaningful connections and turning big ideas into interactive
+        digital experiences.
+      </p>
+      <div className='relative'>
+        <img
+          src={assets.hero_img}
+          alt='hero img'
+          className='w-full max-w-6xl'
+        />
+        <img
+          src={assets.bgImage1}
+          alt='bg img'
+          className='absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden'
+        />
       </div>
     </div>
   );
