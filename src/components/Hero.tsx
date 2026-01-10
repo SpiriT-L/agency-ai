feb61d030b8e26b35717e23c052a816eca941586
LeonID <lmatskevich79@gmail.com>
2026-01-11 00:06:52 +0300

    feat: replace h1 with motion.h1 for animated effect in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,13 +18,18 @@ const Hero = () => {
         <p className='text-xs font-medium'>Trusted by 10k+ people</p>
       </motion.div>
 
-      <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.8 }}
+        viewport={{ once: true }}
+        className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>
         Turning imagination into{' '}
         <span className='bg-linear-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>
           digital
         </span>{' '}
         impact.
-      </h1>
+      </motion.h1>
       <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>
         Creating meaningful connections and turning big ideas into interactive
         digital experiences.
