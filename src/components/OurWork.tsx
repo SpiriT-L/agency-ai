bf840b8471d25140d411e20475ca0032af1320c6
LeonID <lmatskevich79@gmail.com>
2026-01-11 00:48:34 +0300

    feat: enhance OurWork component with motion elements for improved animations

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -1,44 +1,60 @@
-import Title from './Title'
-import assets from '../assets/assets'
+import Title from './Title';
+import assets from '../assets/assets';
+import { motion } from 'motion/react';
 
 const OurWork = () => {
-
-const workData = [
-  {
-    title: 'Mobile app marketing',
-    desc: 'We turn bold ideas into powerful digital solutions that connect...',
-    image: assets.work_mobile_app
-  },
-  {
-    title: 'Dashboard management',
-    desc: 'We turn bold ideas into powerful digital solutions that connect...',
-    image: assets.work_dashboard_management
-  },
-  {
-    title: 'Fitness app promotion',
-    desc: 'We turn bold ideas into powerful digital solutions that connect...',
-    image: assets.work_fitness_app
-  },
-]
+  const workData = [
+    {
+      title: 'Mobile app marketing',
+      desc: 'We turn bold ideas into powerful digital solutions that connect...',
+      image: assets.work_mobile_app,
+    },
+    {
+      title: 'Dashboard management',
+      desc: 'We turn bold ideas into powerful digital solutions that connect...',
+      image: assets.work_dashboard_management,
+    },
+    {
+      title: 'Fitness app promotion',
+      desc: 'We turn bold ideas into powerful digital solutions that connect...',
+      image: assets.work_fitness_app,
+    },
+  ];
 
   return (
-    <div
+    <motion.div
+      initial='hidden'
+      whileInView='visible'
+      transition={{ staggerChildren: 0.2 }}
       id='our-work'
       className='flex flex-col items-center gap-7 px-4 sm:px12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'
     >
-      <Title title='Our latest work' desc='Browse our portfolio of innovative digital projects that showcase creativity, performance, and results.'/>
+      <Title
+        title='Our latest work'
+        desc='Browse our portfolio of innovative digital projects that showcase creativity, performance, and results.'
+      />
 
       <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
-{ workData.map((work, index) => (
-        <div key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
-          <img src={work.image} className='w-full round-xl' alt={work.title} />
-          <h3 className='font-semibold text-lg mt-3 mb-2'>{work.title}</h3>
-          <p className='text-sm opacity-60 w-5/6'>{work.desc}</p>
-          </div>
-))
-}
+        {workData.map((work, index) => (
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+            viewport={{ once: true }}
+            key={index}
+            className='hover:scale-102 duration-500 transition-all cursor-pointer'
+          >
+            <img
+              src={work.image}
+              className='w-full round-xl'
+              alt={work.title}
+            />
+            <h3 className='font-semibold text-lg mt-3 mb-2'>{work.title}</h3>
+            <p className='text-sm opacity-60 w-5/6'>{work.desc}</p>
+          </motion.div>
+        ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
